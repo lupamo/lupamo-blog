62ac733d7d0614794be35fea073e7d197de7f32e
lupamo <arnoldlupamo@gmail.com>
2025-01-12 18:58:58 +0300

    bootstrap responsive

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -8,11 +8,14 @@ import Navbar from 'react-bootstrap/Navbar';
 const Header = () => {
   return (
 	<Navbar expand= "lg" className="bg-body-tertiary, header">
-		<Container className='contain'>
-			<Navbar.Brand href="#home">Lupamo's Blog</Navbar.Brand>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+		<Container fluid className='px-4'>
+			<div className="d-flex align-items-center justify-content-between w-100">
+				<Navbar.Brand href="#home">Lupamo.Dev</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" className='custom-toggler' />
+			</div>
+			
 			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="me-auto">
+				<Nav className="ms-auto">
 					<Nav.Link href="#">Home</Nav.Link>
 					<Nav.Link href="/about">About</Nav.Link>
 					<Nav.Link href="/about">Blogs</Nav.Link>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,7 @@
 import { React } from 'react';
 import Header from '../components/Header';
 import { useEffect, useState } from 'react';
-import portrait from '../assets/portrait-man.png';
+import portrait from '../assets/technology.jpg';
 import '../css/home.css';
 
 const Home = () => {
@@ -15,15 +15,21 @@ const Home = () => {
 	return (
 	  <>
 		<Header />
-		<div className="home" style={{ paddingTop: `${headerHeight}px`, display: 'flex', textAlign: 'center',  width: '100%', height: '100vh' }}>
-			<div style={{height: '100%', flex: '1', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', backgroundColor: '#b23386', border: '1px solid black' }}>
-				<h1 style={{fontSize: '4rem',}}>Welcome to<br></br> Lupamo's Lab</h1>
-			</div>
-		  	<div style={{height: '100%', flex: '1', display: 'grid', justifyContent: 'center', alignItems: 'center'}}>
-			  {<img src={portrait} alt='mascot' className='mascot'></img>}
-			  <h2>Here you will find my projects, blogs, and other stuff</h2>
-			</div>
-		</div>
+		<main className="home" style={{ paddingTop: `${headerHeight}px` }}>
+			<section className="content-section">
+				<h1>Hello, Welcome</h1>
+				<h3 style={{textAlign: 'center'}}>Am Arnold a Web Developer/FrontEnd Engineer</h3>
+				<p style={{fontSize:'1.2rem', textAlign: 'center'}}>
+					I specialize in bringing ideas to life through 
+					clean and efficient code. My focus is on building responsive, dynamic websites that 
+					prioritize performance and user engagement.
+				</p>
+				<button style={{background:'transparent', margin: '1.2rem', padding:'15px 25px', border:'1px solid #fff', color:'#fff'}}>CV download</button>
+			</section>
+			<section className="image-section">
+				<img src={portrait} alt="mascot" className="mascot" />
+			</section>
+		</main>
 	  </>
 	);
   };
